ea46c33c97b17b5dbb6ba139b056cb9f0a6e8088
ThetaSinner <greatdjonfire@hotmail.co.uk>
2020-02-10 23:23:22 +0000

    Initialise repository with netcore projects

---
+++ IngressAgent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IngressAgent
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
