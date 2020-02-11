92c3f92db4f19f735fdd21a9f986a8c3cc3b73c3
ThetaSinner <greatdjonfire@hotmail.co.uk>
2020-02-11 00:43:36 +0000

    Initial ingress rule listener

--- IngressAgent/Program.cs
+++ IngressAgent/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using k8s;
+using k8s.Models;
 
 namespace IngressAgent
 {
@@ -6,7 +9,29 @@ namespace IngressAgent
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = KubernetesClientConfiguration.IsInCluster()
+                ? KubernetesClientConfiguration.InClusterConfig()
+                : KubernetesClientConfiguration.BuildDefaultConfig();
+
+            IKubernetes client = new Kubernetes(config);
+            
+            Console.WriteLine($"Current context {config.CurrentContext}");
+            Console.WriteLine($"Connecting to {client.BaseUri}");
+            
+            var listNodeAsync = client.ListNodeAsync();
+            
+            listNodeAsync.Result.Items.ToList().ForEach(node => Console.WriteLine(node.Metadata.Name));
+
+            var ingressLister = client.ListIngressForAllNamespacesWithHttpMessagesAsync(watch: true);
+            using (ingressLister.Watch<Extensionsv1beta1Ingress, Extensionsv1beta1IngressList>((type, item) =>
+            {
+                Console.WriteLine(type);
+                Console.WriteLine(item.Metadata.Name);
+            }))
+            {
+                Console.WriteLine("Watching for ingress changes...");
+                Console.ReadLine();
+            }
         }
     }
 }
