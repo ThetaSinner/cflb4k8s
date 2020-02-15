c79d11f45198880e6b3015679253d4fa3e07960f
ThetaSinner <greatdjonfire@hotmail.co.uk>
2020-02-15 13:51:35 +0000

    Creat and run mock webapi

---
+++ MockWebApi/Controllers/MockController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MockWebApi.Models;
+
+namespace MockWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MockController : ControllerBase
+    {
+        private readonly ILogger<MockController> _logger;
+
+        public MockController(ILogger<MockController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public MockResponse Get()
+        {
+            _logger.LogInformation("Getting mock response.");
+            
+            return new MockResponse
+            {
+                Name = "mock",
+                Summary = "mock"
+            };
+        }
+    }
+}
---
+++ MockWebApi/Models/MockResponse.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MockWebApi.Models
+{
+    public class MockResponse
+    {
+        public string Name { get; set; }
+        
+        public string Summary { get; set; }
+    }
+}
