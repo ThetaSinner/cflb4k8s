52b2869e40e4bb588010952534015b2b215f03c2
ThetaSinner <greatdjonfire@hotmail.co.uk>
2020-02-15 14:18:42 +0000

    Create mock ingress agent to inject rules into the load balancer

---
+++ MockIngressAgent/ConfigRemoteClient.cs
@@ -0,0 +1,44 @@
+using System;
+using Cflb4K8S;
+using Grpc.Core;
+
+namespace MockIngressAgent
+{
+    public class ConfigRemoteClient
+    {
+        private ConfigRemote.ConfigRemoteClient Client { get; set; }
+        
+        public ConfigRemoteClient(ConfigRemote.ConfigRemoteClient configRemoteClient)
+        {
+            Client = configRemoteClient;
+        }
+
+        public StatusResponse Status()
+        {
+            try
+            {
+                var statusQuery = new StatusQuery();
+                var statusResponse = Client.Status(statusQuery);
+                return statusResponse;
+            }
+            catch (RpcException e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+        public RuleAck PushRule(string host, string target)
+        {
+            var rule = new Rule
+            {
+                Host = host,
+                Target = target
+            };
+
+            var ruleAck = Client.PushRule(rule);
+
+            return ruleAck;
+        }
+    }
+}
---
+++ MockIngressAgent/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using Cflb4K8S;
+using Grpc.Core;
+
+namespace MockIngressAgent
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var channel = new Channel("127.0.0.1:3301", ChannelCredentials.Insecure);
+            var client = new ConfigRemoteClient(new ConfigRemote.ConfigRemoteClient(channel));
+
+            while (true)
+            {
+                var statusResponse = client.Status();
+
+                if (statusResponse == null)
+                {
+                    Console.WriteLine("Service not available.");
+                }
+                else if (!statusResponse.Initialised)
+                {
+                    var ruleAck = client.PushRule("mocka", "https://mock-web-api:5001");
+
+                    if (ruleAck.Accepted)
+                    {
+                        Console.WriteLine("Created rule for 'mocka'");
+                    }
+                }
+                
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
